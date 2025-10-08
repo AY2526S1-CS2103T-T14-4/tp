7b94d24f00be91e996d14511fa5c3414c5c448c8
OKaiXiang <okaixiang@gmail.com>
2025-10-09 00:27:10 +0800

    Add name and person check

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -71,7 +71,7 @@ public class Person {
         }
 
         return otherPerson != null
-                && otherPerson.getName().equals(getName());
+                && otherPerson.getName().fullName.toUpperCase().equals(getName().fullName.toUpperCase());
     }
 
     /**
