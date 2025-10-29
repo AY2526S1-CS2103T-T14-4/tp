0af677c0d55083207e0f382a313f18444b5aa18d
codewallie <142010181+codewallie@users.noreply.github.com>
2025-10-29 18:01:07 +0800

    Update error message

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -28,7 +28,7 @@ public class DeleteCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Deletes the senior identified by the index number, or the full name and phone number "
             + "used in the displayed senior list.\n"
-            + "Parameters: i/INDEX (must be a positive integer) OR n/NAME p/PHONE\n"
+            + "Parameters: i/INDEX (must be a positive integer) OR n/NAME p/PHONE_NUMBER\n"
             + "Examples:\n\t" + COMMAND_WORD + " i/1\n\t" + COMMAND_WORD + " n/Alice Tan p/62345678";
 
     public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Senior: %1$s";
