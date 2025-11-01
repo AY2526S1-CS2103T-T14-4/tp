4e6196002c98308e17aa3385b4f78ca36c17f40b
Swei99 <weilun513@gmail.com>
2025-11-02 01:37:42 +0800

    Add javadocs

--- src/main/java/seedu/address/logic/parser/ClearCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ClearCommandParser.java
@@ -6,6 +6,11 @@ import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 public class ClearCommandParser implements Parser<ClearCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the ClearCommand
+     * and returns a ClearCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
     @Override
     public ClearCommand parse(String args) throws ParseException {
         if (!"--removeall".equals(args.trim())) {
